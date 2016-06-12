19cb563feed1bc8ff8af60aa1882c037dacc0c19
Matthew <matthew.thompson.a@gmail.com>
2016-06-12 18:31:18 -0400

    ng classes hide/show/blur

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,15 @@
 angular.module("todoListApp", [])
 .controller('mainCtrl', function($scope){
-  $scope.helloworld = function(){
-    console.log("hi");
+  $scope.learningNgChange = function(){
+    console.log("an input changed");
   };
+
+  $scope.todos =[
+    {"name": "Task1"},
+    {"name": "Task2"},
+    {"name": "Task3"},
+    {"name": "Task4"},
+    {"name": "Task5"}
+  ]
+
 });
